d703a1f98d1dc2c7cb5cf71bdf0b59a2facf0092
Lénaïc Huard <lenaic@lhuard.fr>
2014-08-24 14:11:37 +0200

    Initial commit
    This is the raw result of scaffolding by “yo angular”.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name privateDockerRegistryBrowserApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the privateDockerRegistryBrowserApp
+ */
+angular.module('privateDockerRegistryBrowserApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name privateDockerRegistryBrowserApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the privateDockerRegistryBrowserApp
+ */
+angular.module('privateDockerRegistryBrowserApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
